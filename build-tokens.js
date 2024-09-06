7e9497e9d5dc60c254fca7a784bd324a96f4e624
Clement <skuankwa@dso.org.sg>
2024-09-06 10:29:04 +0800

    referencing styledic documentation

---
+++ build-tokens.js
@@ -0,0 +1,20 @@
+import StyleDictionary from 'style-dictionary';
+
+// const sd = new StyleDictionary({
+//   source: ['tokens/**/*.json'],
+//   platforms: {
+//     scss: {
+//       transformGroup: 'scss',
+//       buildPath: 'build/',
+//       files: [
+//         {
+//           destination: 'variables.scss',
+//           format: 'scss/variables',
+//         },
+//       ],
+//     },
+//     // ...
+//   },
+// });
+
+// await sd.buildAllPlatforms();
